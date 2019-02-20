95567f6ff40f34cc14dfe4b150ac17aaee2a05a4
Muhamad-Andriansyah <andrsyh7@yahoo.com>
2019-02-20 21:31:43 +0700

    first commit

---
+++ latihan2.py
@@ -0,0 +1,10 @@
+print ('======== latihan 2 ========')
+print ('Menampilkan bilangan, berhenti ketika bilangan 0, dan Menampilkan bilangan terbesar')
+
+b = 0
+while True:
+    angka = int(input("Masukan Bilangan: "))
+    b = angka
+    if b == 0:
+        break
+print ('======== selesai ========')
